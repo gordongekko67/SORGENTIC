a4f005fb88e96ff4e40363f7fe1802698c5ca47e
gordongekko6e <ensa77@yahoo.com>
2024-09-08 12:19:05 +0200

    e

--- Templates_C++/C++alpha/Template/template.cpp
+++ Templates_C++/C++alpha/Template/template.cpp
@@ -1,89 +1,25 @@
-// template.cpp : definisce il punto di ingresso dell'applicazione console.
-//
-
 #include <iostream>
-#include <vector>
+#include <string>
 
 using namespace std;
 
-// funktion
-template<typename T >
-T min(T  a, T b)
-{
-
-	return a < b ? a : b;
-
+template <typename T>
+inline T const& Max (T const& a, T const& b) { 
+   return a < b ? b:a; 
 }
 
-template<typename T >
-void tausche(vector <T>& v)
-{
-	T a = v[0];
-	v[0] = v[1];
-	v[1] = a;
+int main () {
+   int i = 39;
+   int j = 20;
+   cout << "Max(i, j): " << Max(i, j) << endl; 
 
-}
+   double f1 = 13.5; 
+   double f2 = 20.7; 
+   cout << "Max(f1, f2): " << Max(f1, f2) << endl; 
 
+   string s1 = "Hello"; 
+   string s2 = "World"; 
+   cout << "Max(s1, s2): " << Max(s1, s2) << endl; 
 
-double  addiere(int x, double y)
-
-{
-	double erg = 0;
-	erg = x + y;
-	return erg;
-}
-
-template <typename T1, typename  T2>
-T2 addiere(T1 x, T2 y)
-{
-	T2 erg = 0;
-	erg = x + y;
-	return erg;
-}
-
-
-int main()
-{
-	std::vector<int> vI;
-	vector<double>vD;
-	vector<char> vC;
-	
-	for (size_t i = 0; i < 2; i++)
-	{
-		vI[i]=i;
-	}
-	
-	
-
-
-
-	
-
-
-
-	
-
-
-	int a = 3; int  b = 4;
-	float d1 = 3.5, d2 = 5.9;
-	//char c1 = 'g', char c2 = 'u';
-	cout << " Minimun von " << a << b << "  is "  <<std::min(a, b) << endl;
-
-	cout << " addiction mit 2 verschidene " << addiere(a, d1) << endl;
-
-
-	cout << "  " << vI[0] << vI[1] << endl;
-	cout << "  " << vD[0] << vD[1] << endl;
-	cout << "  " << vC[0] << vC[1] << endl;
-
-	tausche(vI);
-	tausche(vD);
-	tausche(vC);
-
-	cout << "  " << vI[0] << vI[1] << endl;
-	cout << "  " << vD[0] << vD[1] << endl;
-	cout << "  " << vC[0] << vC[1] << endl;
-	cout << " adresse" << &a << endl;
-
-	return 0;
+   return 0;
 }
