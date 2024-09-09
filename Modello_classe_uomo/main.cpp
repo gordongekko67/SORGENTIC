23daf6925fb9a06f42cfdf6143be62bc05e2a30b
gordongekko6e <ensa77@yahoo.com>
2024-09-09 12:31:26 +0200

    git

---
+++ Modello_classe_uomo/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include "uomo.h"
+
+using namespace std;
+
+
+
+int main(){
+
+
+    int i =5;
+    uomo Gino("Gino");
+    Gino.mangia(i);
+
+
+
+
+
+    return 0;
+}
---
+++ Modello_classe_uomo/uomo.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+class uomo
+{
+private:
+    /* data */
+public:
+    uomo(std::string nome);
+    ~uomo();
+
+
+
+
+    void mangia(int i){
+
+        std::cout << " adesso mangio" << std::endl; 
+
+    }
+
+};
+
+
