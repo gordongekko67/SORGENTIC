885630300ebc708929d8b0bcbf917de02308c37d
gordongekko6e <ensa77@yahoo.com>
2024-10-10 08:15:15 +0200

    correzioe 10/10?24

--- Corso_James_Reynard/ssstreams4.cpp
+++ Corso_James_Reynard/ssstreams4.cpp
@@ -32,7 +32,7 @@ int main()
         if (found1 != std::string::npos)
         {
             std::string campo1 = line.substr(0, (found1)); // Language name
-            std::cout << std::setw(15) << std::left << campo1 << "  ";
+            std::cout << std::setw(15) << std::left << campo1 << ",";
         }
         else
         {
@@ -45,7 +45,7 @@ int main()
             {
                 std::string campo2 = line.substr(found1 + 1, i - found1 - 1); // Designer
                 std::string campo3 = line.substr(i, 4); // Year
-                std::cout << std::setw(20) << std::left << campo2;
+                std::cout << std::setw(20) << std::left << campo2 << ",";
                 std::cout << std::setw(4)  << campo3 << "\n";
                 break;
             }
